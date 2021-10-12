c7155ba5712a0230c0a341b619cb47cdb78bca7f
GERMAN LAVANDERA <jdgerman@hotmail.com>
2021-10-11 23:05:10 -0700

    cambios

---
+++ dist/precache-manifest.39b32bcafb5445d6ea73b526882276ab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7b570c3e250e60ca47",
+    "url": "/pokedex/css/app.e7297fdd.css"
+  },
+  {
+    "revision": "8e2de9104aec1f29795c",
+    "url": "/pokedex/css/chunk-vendors.6666a008.css"
+  },
+  {
+    "revision": "b54eaa6bbd3fe4066ea3af1712e21963",
+    "url": "/pokedex/img/bg.b54eaa6b.svg"
+  },
+  {
+    "revision": "01a2fb22b9e2f628c2b8e1f96b9ec636",
+    "url": "/pokedex/img/logopokemon.01a2fb22.png"
+  },
+  {
+    "revision": "f242eb6dc59591462340436290a0c7f7",
+    "url": "/pokedex/img/pokebola.f242eb6d.png"
+  },
+  {
+    "revision": "f7d7512ffffb048e54ff773d9591ee25",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "ed7b570c3e250e60ca47",
+    "url": "/pokedex/js/app.8523ada1.js"
+  },
+  {
+    "revision": "8e2de9104aec1f29795c",
+    "url": "/pokedex/js/chunk-vendors.cea69bd1.js"
+  },
+  {
+    "revision": "5a74c2a26db4a6efc1afaae6e027b7af",
+    "url": "/pokedex/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pokedex/robots.txt"
+  }
+]);
